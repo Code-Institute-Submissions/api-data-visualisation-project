9c046f863507973e7213f121d842976963130d23
Steven John Bolton <steviebolton@gmail.com>
2018-10-30 20:11:00 +0000

    styling & code cleaned, read me completed

---
+++ README.md
@@ -0,0 +1,38 @@
+
+## Shark Facts
+#### by Steve Bolton
+
+The stories we hear about sharks are numerous and more often than not they teach us to be afraid of Sharks.
+But just how afraid should we be. Should we believe any of the stories? Making scary films about sharks has become a multi million dollar industry. The truth is people are very seldom attacked by Sharks. Sharks normaly feed on fish how ever occasionally Sharks have been spotted close to the coast sometimes in bathing areas, some say it may have to do with the rise in temperature. What ever we want to believe it is worth taking a look at real statistics. With a few searches we were able to find some data here:
+https://knoema.com/lftyaag/confirmed-unprovoked-shark-attacks-around-the-world?action=export&gadget=visualization
+http://www.convertcsv.com/csv-to-json.htm
+This dashboard shows proportionally and numerically how many unproked shark attacks have been registered since 1503. If we had based the charts on the number of fatalities we would not have much to show.
+
+#### Features
+This project pulls it's data from a JSON file which was downloaded as a csv file
+https://knoema.com/lftyaag/confirmed-unprovoked-shark-attacks-around-the-world?action=export&gadget=visualization# which was then converted to JSON format. This was done using this website:
+http://www.convertcsv.com/csv-to-json.htm
+
+By clicking on the pie chart or the bar chart the dc.crossfilter kicks in and the surrounding charts will dymamically
+respond accordingly.
+ 
+#### Features Left to Implement
+other interesting information about Sharks,
+Photo library.
+
+#### Technologies Used
+Html, CSS, javascript libraries: dc and d3.
+
+#### This project uses the following libraries:
+- [dc,js](https://dc-js.github.io/dc.js/) a dimensional charting javascrpt library with native crossfilter support, allowing highly efficient exploration on large multi-dimensional datasets.
+
+- [d3.js](https://d3js.org/) is a JavaScript library for manipulating documents based on data. D3 helps you bring data to life using HTML, SVG, and CSS. D3’s emphasis on web standards gives you the full capabilities of modern browsers without tying yourself to a proprietary framework, combining powerful visualization components and a data-driven approach to DOM manipulation.
+
+I wrote the CSS to create the dashboard. It has only been tested on a google browser. Note this dash board is not intended to be responsive.
+The background image was edited in photoshop by yours truly (that is me by the way).
+
+### Credits
+Thankyou Code Institute and especially to Mr. Richard Dalton and Mr. Matt Rudge.
+
+
+
--- static/js/shark-attacks.js
+++ static/js/shark-attacks.js
@@ -1,126 +1,23 @@
 
-queue()
+queue() 
+
 
         .defer(d3.json, "static/data/shark-attacks.json")
         .await(makeGraphs);
-
+        
         function makeGraphs(error, sharkattackdData) {
         var ndx = crossfilter(sharkattackdData);
 
-        var name_dim = ndx.dimension(dc.pluck('World Region'));
-        var casulaties = name_dim.group().reduceSum(dc.pluck('Total Number of Attacks'));
+        /*------css-chartA-------*/  
         
-        dc.barChart("#shark-attacks-chart-region")
-        
-            .width(600)
-            .height(500)
-            .margins({top: 200, right: 50, bottom: 30, left: 50})
-            .dimension(name_dim)
-            .group(casulaties)
-            .transitionDuration(500)
-            .x(d3.scale.ordinal())
-            .xUnits(dc.units.ordinal)
-            .xAxisLabel("World Region")
-            .yAxis().ticks(4);
+            var name_dim = ndx.dimension(dc.pluck('Country'));
+            var casulaties = name_dim.group().reduceSum(dc.pluck('Total Number of Attacks'));
+    
+            dc.barChart("#shark-attacks-chart-region")
             
-            
-            
-            dc.renderAll();
-            
- /*----------------------------------------------------*/  
-
-        var casulaties = name_dim.group().reduceSum(dc.pluck('Number of Non Fatal Attacks'));
-        
-        dc.barChart("#shark-attacks-chart-country")
-            .width(1024)
-            .height(300)
-            .margins({top: 0, right: 50, bottom: 30, left: 50})
-            .dimension(name_dim)
-            .group(casulaties)
-            .transitionDuration(500)
-            .x(d3.scale.ordinal())
-            .xUnits(dc.units.ordinal)
-            .xAxisLabel("")
-            .yAxis().ticks(4);  
-            
-        var name_dim = ndx.dimension(dc.pluck('Total Number of Attacks'));
-        var casulaties = name_dim.group().reduceSum(dc.pluck('Country'));
-        
-  
-        
-        
-  /*----------------------------------------------------------------------*/ 
-
-        dc.pieChart('#pie-chart')
- 
-            .transitionDuration(1500)
-            .dimension(name_dim)
-            .group(casulaties);
-
-        var state_dim = ndx.dimension(dc.pluck('World Region'));
-        var casulaties_attacks_region = state_dim.group().reduceSum(dc.pluck('Total Number of Attacks'));
-
-        dc.pieChart('#pie-chart')
-            .height(300)
-            .radius(600)
-            .transitionDuration(1500)
-            .dimension(state_dim)
-            .group(casulaties_attacks_region);    
-            
-            
-            
-
-   /*----------------------------------------------------------------------*/ 
-   
-        var name_dim = ndx.dimension(dc.pluck('name'));
-
-        var spendByNameStoreA = name_dim.group().reduceSum(function (d) {
-                if (d.store === 'A') {
-                    return +d.spend;
-                } else {
-                    return 0;
-                }
-            });
-        var spendByNameStoreB = name_dim.group().reduceSum(function (d) {
-                if (d.store === 'B') {
-                    return +d.spend;
-                } else {
-                    return 0;
-                }
-            });
-
-        var stackedChart = dc.barChart("stacked-chart");
-         
-        stackedChart
-            .width(500)
-            .height(500)
-            .dimension(name_dim)
-            .group(spendByNameStoreA, "Store A")
-            .stack(spendByNameStoreB, "Store B")
-            .x(d3.scale.ordinal())
-            .xUnits(dc.units.ordinal)
-            .legend(dc.legend().x(420).y(0).itemHeight(15).gap(5));
-
-        stackedChart.margins().right = 100;
-        
-        
-        
-        
-/*----------------------------------------------------------------------------*/ 
-
-        
-
-
-        var name_dim = ndx.dimension(dc.pluck('Country'));
-        var casulaties = name_dim.group().reduceSum(dc.pluck('Total Number of Attacks'));
-        
-            
-
-        dc.barChart("#shark-attacks-chart-region")
-            
-            .width(1750)
+            .width(1700)
             .height(600)
-            .margins({top: 50, right: 100, bottom: 150, left: 100})
+            .margins({top: 0, right: 100, bottom: 150, left: 100})
             .mouseZoomable(true)
             .dimension(name_dim)
             .group(casulaties)
@@ -129,11 +26,45 @@ queue()
             .xUnits(dc.units.ordinal)
             .xAxisLabel("")
             .yAxis().ticks(5);
-
+   
+        /*------css-chartB-------*/  
         
+            var state_dim = ndx.dimension(dc.pluck('World Region'));
+            var casulaties_attacks_region = state_dim.group().reduceSum(dc.pluck('Total Number of Attacks'));
+
+            dc.pieChart('#pie-chart')
+            
+            .transitionDuration(1000)
+            .dimension(name_dim)
+            .group(casulaties)
+            .height(300)
+            .radius(600)
+            .transitionDuration(1500)
+            .dimension(state_dim)
+            .group(casulaties_attacks_region);    
+  
+        /*------css-chartC-------*/ 
+
+        var name_dim = ndx.dimension(dc.pluck('World Region'));
+        var casulaties = name_dim.group().reduceSum(dc.pluck('Total Number of Attacks'));
+        
+        dc.barChart("#shark-attacks-chart-country")
+            .width(1024)
+            .height(300)
+            .margins({top: 0, right: 50, bottom: 50, left: 50})
+            .dimension(name_dim)
+            .group(casulaties)
+            .transitionDuration(500)
+            .x(d3.scale.ordinal())
+            .xUnits(dc.units.ordinal)
+            .xAxisLabel("")
+            .yAxis().ticks(4);  
 
 
-
-
-        dc.renderAll();
+    dc.renderAll(); 
+    
      };
+     
+     
+     
+
